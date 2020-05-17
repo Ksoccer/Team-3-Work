56954adb448a2bfa16fc3810e570f4a2a01ecf3a
kvnithinkumar <65356301+kvnithinkumar@users.noreply.github.com>
2020-05-17 12:47:07 -0500

    Update Nithin.c

--- Nithin.c
+++ Nithin.c
@@ -1,7 +1,39 @@
 #include<stdio.h>
+
 int main()
 {
-  printf("Hello World");
+  float Current,Voltage,Frequency;
+  char OverCurrentFault,OverVoltageFault,UnderCurrentFault,UnderVoltageFault,OverFrequencyFault;
+  
+  printf("Enter Current")
+  scanf("%d",&Current);
+  printf("Enter Voltage")
+  scanf("%d",&Current);
+  printf("Enter Frequency")
+  scanf("%d",&Frequency);
+  
+  //Invoke fault functions
+  if(OverCurrentFault)
+  {	
+    printf("OC Fault");
+  }
+  if(OverVoltageFault)
+  {	
+    printf("OC Fault");
+  }
+  if(UnderCurrentFault)
+  {	
+    printf("OC Fault");
+  }
+  if(UnderVoltageFault)
+  {	
+    printf("OC Fault");
+  }
+  if(OverCurrentFault)
+  {	
+    printf("OC Fault");
+  }
+   
   return 0;
 }
 
