e60fdaf90e46b3fc7ea47e0bd485de4516affb2e
subha2019 <65356460+subha2019@users.noreply.github.com>
2020-05-17 23:27:20 +0530

    Update Detect_UnderVoltage.c

--- Detect_UnderVoltage.c
+++ Detect_UnderVoltage.c
@@ -1,22 +1,10 @@
 #include <stdio.h>
 #include "Faults.h"
 
-bool UnderVoltage(int PHA_V,int PHB_V,int PHC_V, int time)
+bool UnderVoltage(int voltage, int time)
 {
-   int PHUV;
-   int flag_undervolatge;
- 
-    if (PHA_V <= PHB_V) {
-        PHUV = PHA_V;
-    } else {
-        PHUV = PHB_V;
-    }
-    if (PHUV <= PHC_V) {
-    } else {
-       PHUV = PHC_V;
-    }
- 
- if(PHUV<K_UV_MAX && time==20)
+
+ if(voltage<K_UV_MAX && time==20)
  {
    return = 1; 
  } 
