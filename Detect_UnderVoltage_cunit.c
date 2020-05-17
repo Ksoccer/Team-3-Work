50a9c1c3db9c11abf9ee73032aac1fdc99490cd3
subha2019 <65356460+subha2019@users.noreply.github.com>
2020-05-18 01:07:05 +0530

    Update Detect_UnderVoltage_cunit.c

--- Detect_UnderVoltage_cunit.c
+++ Detect_UnderVoltage_cunit.c
@@ -1,21 +1,24 @@
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
+#include "Faults.h"
 
 int maxi(int i1, int i2)
 {
     return (i1 > i2) ? i1 : i2;
 }
 
-void test_maxi(void)
+void test_UnderVoltage(void)
 {
-    CU_ASSERT(maxi(0,2) == 2);
+    CU_ASSERT(UnderVoltage(9.0,20) == 1);
+    CU_ASSERT(UnderVoltage(10.0,20) == 0);
+    CU_ASSERT(UnderVoltage(9.0,19) == 0);
 }
 
 int main() {
     CU_initialize_registry();
-    CU_pSuite suite = CU_add_suite("maxi_test", 0, 0);
+    CU_pSuite suite = CU_add_suite("test_UnderVoltage", 0, 0);
 
-    CU_add_test(suite, "maxi_fun", test_maxi);
+    CU_add_test(suite, "test_UnderVoltage", test_UnderVoltage);
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
