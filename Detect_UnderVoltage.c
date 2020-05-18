ec52d40d59bbc8eb11d686c0641c142bef43f74e
subha2019 <65356460+subha2019@users.noreply.github.com>
2020-05-19 04:49:01 +0530

    Update Detect_UnderVoltage.c
    Adding Description

--- Detect_UnderVoltage.c
+++ Detect_UnderVoltage.c
@@ -1,10 +1,31 @@
+/*********************************************************************************
+ *
+ *                      ********************************
+ *                      *                              *
+ *                      *     Detect_UnderVolatge.c
+ *                      *                              *
+ *                      ********************************
+ *
+ ********************************************************************************
+ *
+ * Author(s): Subhakanta Swain
+ *
+ ********************************************************************************
+ * Description:
+ *
+ * Detect UnderVoltage when the Volatge value is less than threshold for time
+ *
+ *
+ ********************************************************************************/
+/* included library and header file: */
 #include <stdio.h>
 #include "Faults.h"
 #include <stdbool.h>
 
 bool UnderVoltage(float voltage, int time)
 {
-
+ 
+ //Checking the Voltage value if less than K_UV_MAX(define in Faults.h) for time 20
  if(voltage<K_UV_MAX && time==20)
  {
    return 1; 
