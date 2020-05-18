54ded202c75b2d4be85b7f47ba0218349b2aacaa
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-05-18 08:53:36 -0500

    Update OverVoltageFault.c

--- OverVoltageFault.c
+++ OverVoltageFault.c
@@ -1,14 +1,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "Faults.h"
-#include <CUnit/CUnit.h>
-#include <CUnit/Basic.h>
 
-bool OverV(int MaxV, int ReadV){
-	if (MaxV > ReadV){
-		return False;
+bool OverVoltage(float voltage, int time){
+	if (voltage > K_OV_MAX && time == 20){
+		return 1;
 	}
 	else {
-		return True;
+		return 0;
 	}
 }
