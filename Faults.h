c711ba2939770b963332684d91c9d9c508309484
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-05-18 09:03:52 -0500

    Update Faults.h

--- Faults.h
+++ Faults.h
@@ -7,9 +7,11 @@
 #define K_UF_MIN 350.0
 #define K_OC_MIN 10.0
 #define K_OF_MAX 450.0
+#define K_OV_MAX 15.0
 
 bool UnderVoltage(float voltage,int time);
 bool UnderCurrent(float current,int time);
 bool OverFrequency(float Frequency, int time);
 bool UnderFrequency(float Frequency, int time);
 bool OverCurrent(float current, int time);  
+bool OVerVoltage(float voltage, int time);
