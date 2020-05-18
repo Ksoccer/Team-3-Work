adc2d4aca9042186e24c15e6eee8afb1a5b91d46
sambasivarao0714 <65406306+sambasivarao0714@users.noreply.github.com>
2020-05-18 07:53:57 -0500

    Update Faults.h
    Added Over Frequency function declaration

--- Faults.h
+++ Faults.h
@@ -6,5 +6,9 @@
 #define K_UI_MIN 1.0
 #define K_UF_MIN 350
 
+#define K_OF_MAX 450.0
+
 bool UnderVoltage(float voltage,int time);
 bool UnderCurrent(float current,int time);
+bool OverFrequency(float Frequency, int time);
+  
