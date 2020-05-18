3462f2d58b5c00861e934449b5940ff1b021c9a4
Ksoccer <65356275+Ksoccer@users.noreply.github.com>
2020-05-18 11:09:02 -0500

    Update Over_Current.c
    Updated and added comments

--- Over_Current.c
+++ Over_Current.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/* #include <stdio.h>
 #include "Faults.h"
 
 bool UnderCurrent(int PHA_I,int PHB_I,int PHC_I, int time)
@@ -22,4 +22,21 @@ bool UnderCurrent(int PHA_I,int PHB_I,int PHC_I, int time)
  } 
  else()
    return = 0;
+}
+*/
+#include <stdio.h>
+#include "Faults.h"
+#include <stdbool.h>
+
+bool OverCurrent(float Current, int time) /* Brings in values from bolean OverCurrent of current and time. */
+{
+
+ if(Current>K_OC_MAX && time==20) /* checks is current value is greater than the max allowable current and if time is set to 20. If current greater than max returns a true and reports the fault */
+ {
+   return 1; 
+ }
+ else
+ {
+   return 0;
+ }
 }
