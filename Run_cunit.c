753545fadf0882851f99af622fceb3fbe8631f4e
Ksoccer <65356275+Ksoccer@users.noreply.github.com>
2020-05-17 23:42:02 -0500

    Update Run_cunit.c

--- Run_cunit.c
+++ Run_cunit.c
@@ -14,6 +14,16 @@ void test_UnderFrequency(void)
     CU_ASSERT(UnderFrequency(350.0,20) == 0);
     CU_ASSERT(UnderFrequency(340.0,19) == 0);
 }
+
+void test_OverCurrent(void)
+{
+    CU_ASSERT(OverCurrent(11,20) == 1);
+    CU_ASSERT(OverCurrent(10,20) == 0);
+    CU_ASSERT(OverCurrent(11,19) == 0);
+}
+
+
+
 int main()
 {
     CU_initialize_registry();
@@ -24,6 +34,10 @@ int main()
 	CU_pSuite suite = CU_add_suite("test_UnderFrequency", 0, 0);
 
     CU_add_test(suite, "test_UnderFrequency", test_UnderVoltage);
+	
+	CU_pSuite suite = CU_add_suite("test_OverCurrent", 0, 0);
+
+    CU_add_test(suite, "test_OverCurrent", test_OverCurrent);
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
