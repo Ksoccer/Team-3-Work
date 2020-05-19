8ee31dcd98ae8e74ca4a2d9da635ce071d7892ff
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-05-18 20:00:48 -0500

    Update OverVoltageFault.c

--- OverVoltageFault.c
+++ OverVoltageFault.c
@@ -1,9 +1,11 @@
 /*********************************
 	Over Voltage Test
 	
-takes in a float, voltage, and compares it
-to the maximum overvoltage
-
+takes in a float, voltage, and 
+compares it to the maximum voltage 
+that would trigger a fault in the 
+system over a set time, in this
+case that set time being 20 seconds
 
 *********************************/
 
