8fa1d193598c6b9994aa1cb7662fcc3a3c890f90
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-05-24 23:17:34 -0500

    Update BinarySearch.c

--- BinarySearch.c
+++ BinarySearch.c
@@ -1,5 +1,16 @@
 #include <stdio>
 #include <stdlib>
 
-int searchtree ()
+int SearchArray [];
+int beginning;
+int end;
+int x
+
+//searches a sorted array for int x
+int searchtree (SearchArray[], beginning, end, x){
+	while (beginning <= end){
+		int middle = 1 + (end - 1) / 2;
+		
+	} 
+}
 
