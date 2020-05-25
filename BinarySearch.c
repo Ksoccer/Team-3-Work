f15b2cbee0261c5eae94646175d15f403bf4cb7d
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-05-25 14:11:09 -0500

    Update BinarySearch.c

--- BinarySearch.c
+++ BinarySearch.c
@@ -1,7 +1,8 @@
 #include <stdio>
 #include <stdlib>
+#include "Faults.h"
 
-int SearchArray [];
+int FaultArray [];
 int beginning;
 int end;
 int x
@@ -9,8 +10,28 @@ int x
 //searches a sorted array for int x
 int searchtree (SearchArray[], beginning, end, x){
 	while (beginning <= end){
-		int middle = 1 + (end - 1) / 2;
+	//identifies the middle of the array
+		int middle = beginning + (end - beginning) / 2;
+	//checks if int is the middle integer in the array
+		if (FaultArray [middle == x]){
 		
+			return middle;
+			
+		}
+		//checks if the int x is in the end half of the array
+		else if ( FaultArray[middle] < x ){
+		
+			beginning = middle + 1;
+		
+		}
+		// checks if int x is in the beginning half of the array
+		else if ( FaultArray[middle] > x){
+		
+			end  = middle - 1;
+		
+		}
 	} 
+	//if int x is not in the array
+	return -1;
 }
 
