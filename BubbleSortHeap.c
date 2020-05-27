06158b08b7f8923020f447db2cb550de912ea59e
subha2019 <65356460+subha2019@users.noreply.github.com>
2020-05-27 05:42:15 +0530

    Update BubbleSortHeap.c

--- BubbleSortHeap.c
+++ BubbleSortHeap.c
@@ -2,39 +2,9 @@
 #include<stdbool.h>
 #include "Faults.h"
 #include <stdlib.h>
-void swap(int *xp, int *yp) 
-{ 
-    int temp = *xp; 
-    *xp = *yp; 
-    *yp = temp; 
-} 
-
-/* Function to print an array */
-void printArray(int arr[], int size) 
-{ 
-    int i; 
-    for (i=0; i < size; i++) 
-        printf("%d ", arr[i]); 
-    printf("\n"); 
-} 
-  
-// A function to implement bubble sort 
-void bubbleSort(int arr[], int n) 
-{ 
-   
-   int i, j; 
-   for (i = 0; i < n-1; i++)       
-  
-       // Last i elements are already in place    
-       for (j = 0; j < n-i-1; j++)  
-           if (arr[j] > arr[j+1]) 
-              swap(&arr[j], &arr[j+1]); 
-} 
 
 // A function to implement bubble sort 
-void bubbleSort_Heap(int arr[], int n){ 
-    
- 
+void bubbleSort_Heap(int arr[], int n){   
  printf("Before Sorting of array: \n"); 
  printArray(arr, n);	
  bubbleSort(arr, n); 
