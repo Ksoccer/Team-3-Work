50a195e4f08f4cf835eba0d52b35f9ae6e69e990
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-05-27 12:52:40 -0500

    Update OverVoltageFaultFunctionPointer.c

--- OverVoltageFaultFunctionPointer.c
+++ OverVoltageFaultFunctionPointer.c
@@ -29,13 +29,16 @@ int pointers(){
 	
 }
 bool OverVoltage(voltage, time){
+	
+	Fpoint = &OverVoltage();
+	
 	if (voltage > K_OV_MAX && time == 20){
 		return 1;
 	}
 	else {
 		return 0;
 	}
-	Fpoint = &OverVoltage();
+	
 }
 
 
