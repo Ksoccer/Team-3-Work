1f2cf4d70c155f05f1ff4e74063c903baff3b3fa
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-05-28 09:52:45 -0500

    Update Main.c

--- Main.c
+++ Main.c
@@ -83,7 +83,7 @@ int main()
 	
 /***************** Binary Search **********************/
 // uncomment this set of code and the relating code in the .travis.yml to build
-/*
+	
 int FaultArray[] = {33, 45, 46, 51, 60, 67, 71, 80, 88};
 int beginning = FaultArray[0];
 int end = FaultArray [sizeof (FaultArray) / sizeof (FaultArray[0])];
@@ -101,7 +101,7 @@ int HeapEnd = *(HeapArray + sizeof(*(HeapArray))/sizeof(*(HeapArray + 0)));
 int HeapMiddle  = *(HeapArray + HeapEnd/2);
 BinarySearchHeap(HeapArray, HeapBeginning, HeapEnd, y);
 free(HeapArray);
-*/
+
 
  /*************** Bubble Sort ***************************/	
 /*int arr1[] = {33, 11, 22, 77, 66, 44, 55}; 
@@ -119,7 +119,7 @@ for(int p=0;p<n1;p++)
 }
 //calling bubbleSort_Stack function using Dynamic(heap) memory values
 bubbleSort_Heap(arr, n1);
-free(arr); */
+free(arr); 
  return 0;
 }
-
+*/
