c95da65e589096bc0076a1dbe982eeb2134156de
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-06-01 21:23:11 -0500

    Update Main.c

--- Main.c
+++ Main.c
@@ -101,7 +101,7 @@ for (int i = 0; i <= heapsize; i++){
 int HeapBeginning  = *(HeapArray + 0);
 int HeapEnd = *(HeapArray + sizeof(*(HeapArray))/sizeof(*(HeapArray + 0)));
 int HeapMiddle = *(HeapArray + HeapEnd/2);
-void HeapSearch(int *HeapArray, int HeapBeginning, int HeapEnd, int y);
+int HeapSearch(int *HeapArray, int HeapBeginning, int HeapEnd, int y);
 int threadsearch = HeapSearch(HeapArray, HeapBeginning, HeapEnd, y);
 //threading the binary search heap function
 pthread_t thr1, thr2, thr3;
