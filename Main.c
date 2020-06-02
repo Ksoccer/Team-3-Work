22fae0b64e9426d0fc708442c07067aa2437f71e
rfurlr <65356353+rfurlr@users.noreply.github.com>
2020-06-01 20:10:03 -0500

    Update Main.c

--- Main.c
+++ Main.c
@@ -92,7 +92,7 @@ int middle  = FaultArray[end/2];
 int x = 45;
 int binarysearch(int FalutArray [], int beginning, int end, int x);
 int heapsize = 11;
-int *HeapArray = (int*) malloc (heapsize*sizeof(int));
+*HeapArray = (int*) malloc (heapsize*sizeof(int));
 int array[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110};
 int y = 10;
 for (int i = 0; i <= heapsize; i++){
